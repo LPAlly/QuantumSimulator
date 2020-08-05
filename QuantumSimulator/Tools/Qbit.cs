18920785e9617631f1ff11faffac0bc649b873bf
LPAlly <louisphilip76x@hotmail.com>
2020-08-05 14:28:24 -0400

    Added qbit classes

---
+++ QuantumSimulator/Tools/Qbit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace QuantumSimulator.Tools
+{
+    public class Qbit
+    {
+        public Matrix<double> QbitState;
+
+        public Qbit(double zeroStateValue, double oneStateValue)
+        {
+            QbitState = Matrix<double>.Build.DenseOfColumnArrays(new[] { zeroStateValue }, new[] { oneStateValue });
+        }
+    }
+}
